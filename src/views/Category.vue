5ec60af642f654860723618cf2e5961d8dde4f95
liuchang <1746571816@qq.com>
2019-07-25 15:27:11 +0800

    分类完成

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -5,17 +5,21 @@
       <van-row>
         <van-col span="6" class="nav">
           <ul>
-            <li @click="selectCategory(item.typeId,index)" :class="{active:active==index}" v-for="(item,index) in types" :key="index" >{{item.typeName}}</li>
-          </ul> 
+            <li :class="{active:active==index}" @click="selectCategory(item.typeId,index)" v-for="(item,index) in types"
+              :key="index">{{item.typeName}}</li>
+          </ul>
         </van-col>
         <van-col span="18" class="container">
-          <van-list class="content" >
-            <div class="content-item" v-for="(item ,index) in productList" :key="index">
-              <img :src="item.img" alt="">
-              <p class="content-item-name" >{{item.name}}</p>
-              <p>￥{{item.price}}</p>
-            </div>
-          </van-list>
+          <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+            <van-list v-model="isLoading" class="content" @load="onLoad" :finished="finished" finished-text="没有更多了">
+              <div class="content-item" v-for="(item , index) in productList" :key="index">
+                <img :src="item.img" alt="">
+                <p class="content-item-name">{{item.name}}</p>
+                <p>￥{{item.price}}</p>
+              </div>
+            </van-list>
+          </van-pull-refresh>
+
         </van-col>
       </van-row>
     </div>
@@ -25,47 +29,71 @@
 <script>
   import axios from "axios";
   import url from '@/service.config.js'
+  import {
+    setTimeout
+  } from 'timers';
   export default {
-    data(){
-      return{
-        types:[],
-        productList:[],
-        active:0,
-        typeId:1,//当前选中类型的id
-        start:0,
-        limit:10
+    data() {
+      return {
+        types: [],
+        productList: [],
+        active: 0,
+        typeId: 1, //当前选中类型的id
+        start: 0,
+        limit: 10,
+        finished: false,
+        isLoading:false
       }
     },
-    methods:{
-      selectCategory(typeId,index){
+    methods: {
+      selectCategory(typeId, index) {
         this.active = index;
         this.typeId = typeId;
         this.getProductList();
+        this.productList = [];
+        this.finished = false;
       },
-      getProductList(){
+      getProductList() {
+          this.isLoading = true;
         axios({
-          url:url.getProductByType,
-          method:'get',
-          params:{
-            typeId:this.typeId,
-            start:this.start,
-            limit:this.limit
+          url: url.getProductByType,
+          method: 'get',
+          params: {
+            typeId: this.typeId,
+            start: this.productList.length,
+            limit: this.limit
           }
-        }).then((res)=>{
-          this.productList = res.data;
-        }).catch(()=>{
+        }).then((res) => {
+          this.isLoading = false;
+          if (res.data.length != 0) {
+            this.productList = this.productList.concat(res.data);
+          } else {
+            this.finished = true;
+          }
+        }).catch(() => {
 
         })
+      },
+      onLoad() {
+        setTimeout(() => {
+          this.getProductList();
+        }, 2000)
+      },
+      onRefresh(){
+        setTimeout(() => {
+          this.productList = [];
+          this.getProductList();
+        }, 2000);
       }
     },
 
     created() {
       axios({
-          url:url.getTypes,
+          url: url.getTypes,
         })
         .then(res => {
           this.types = res.data;
-          this.selectCategory(this.typeId,this.index)
+          this.selectCategory(this.typeId, this.active);
           console.log(this.types);
         })
         .catch(err => {
@@ -76,38 +104,53 @@
 </script>
 
 <style lang="scss">
-.nav{
-  background: #eee;
-  li{
-    height: 0.6rem;
-    line-height: 0.6rem;
-    border-bottom: 1px solid #fff;
-    text-align: center;
-    padding: 3px;
-  }
-  .active{
-    background: #ffffff;
-  }
-}
-.container{
-  position: fixed;
-  top:1rem;
-  right: 0;
-  bottom:1rem;
-  overflow-y:scroll;
-}
-.content{
-  display:flex;
-  flex-wrap:wrap;
-  padding-bottom:1rem;
-  &-item{
-    width:40%;
-    padding: 0 10px;
-    text-align: center; 
-    img{
-      width: 2rem;
-      height: 2rem;
+  .nav {
+    background: #eee;
+
+    li {
+      height: 0.6rem;
+      line-height: 0.6rem;
+      border-bottom: 1px solid #fff;
+      text-align: center;
+      padding: 3px;
+    }
+
+    .active {
+      background: #ffffff;
     }
   }
-}
+
+  .container {
+    background: #ffffff;
+    position: fixed;
+    top: 46px;
+    right: 0;
+    bottom: 1rem;
+    overflow-y: scroll;
+  }
+
+  .content {
+    display: flex;
+    flex-wrap: wrap;
+    padding-bottom: 1rem;
+
+    &-item {
+      width: 40%;
+      padding: 0 10px;
+      text-align: center;
+
+      img {
+        width: 2rem;
+        height: 2rem;
+      }
+
+      &-name {
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 2;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
+    }
+  }
 </style>
